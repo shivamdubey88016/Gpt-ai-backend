ba0a1517131b8c0ba4118d99dcf0d8b25c23ab22
shivamdubey88016 <shivamdubey88016@gmail.com>
2026-02-11 01:31:02 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 
 const connectDb=async ()=>{
